2a4328e9f89a65a73435b35b746dcd549fd09ee1
devinshade <devindanielle93@gmail.com>
2024-02-25 14:13:13 -0700

    updated mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,78 @@
-/* mutations.js:
+// mutations.js:
 
-LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+// initial setup for Apollo Client for React
+import { gql } from '@apollo/client';
 
-ADD_USER will execute the addUser mutation.
+// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
 
-SAVE_BOOK will execute the saveBook mutation.
+// ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    authors
+                    bookId
+                    image
+                    link
+                    title
+                    description
+                }
+            }
+        }
+    }
+`;
 
-REMOVE_BOOK will execute the removeBook mutation. */
+// SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql`
+    mutation saveBook($newBook: InputBook!) {
+        saveBook(newBook: $newBook) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+// REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
