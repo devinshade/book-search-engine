f44a577df0871c2c1610b49b8af2897a1d7248c0
devinshade <devindanielle93@gmail.com>
2024-02-25 13:11:34 -0700

    udpated auth middleware function to work with graphql api

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,19 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  // ! TODO: Update the auth middleware function to work with the GraphQL API
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+    
+    // if no token passed, return the req
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +27,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(400).json({ message: 'Invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return the request object 
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
